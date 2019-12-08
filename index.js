fba3fc6146daea51df770f80c9d7a090a92aa005
Lee Bao <leebao101@gmail.com>
2019-12-08 23:13:41 +0700

    View engine pug

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var app = express();
+
+app.set('view engine','pug');
+app.set('views', './views');
+
 var port = 3000;
 
 app.get('/', function(req,res){
-	res.send('<h1>Hello Lee Bảo</h1>');
+	res.render('index', {
+		name: "Lee Bảo"
+	});
 });
 
+app.get('/users', function(req,res){
+	res.render('users/index',{
+		users: [
+		{id: 1, name: 'Bảo'},
+		{id: 2, name: 'Hảo'}
+		]
+	});
+});
 
 
 app.listen(port, function(){
